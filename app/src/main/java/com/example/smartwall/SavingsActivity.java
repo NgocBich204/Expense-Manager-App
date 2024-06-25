76bd460f77447c6016ef26e2e9ef0b0710ed2d47
Nguyen Thi Ngoc Bich <93378230+NgocBich204@users.noreply.github.com>
2024-06-25 17:12:20 +0700

    upadteMenuItem

---
+++ app/src/main/java/com/example/smartwall/SavingsActivity.java
@@ -0,0 +1,26 @@
+package com.example.smartwall;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SavingsActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_savings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.saving), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

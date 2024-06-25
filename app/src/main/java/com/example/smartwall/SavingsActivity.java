87fbcc361b9f49909634096a92f75416abaa13be
Nguyen Thi Ngoc Bich <93378230+NgocBich204@users.noreply.github.com>
2024-06-26 00:45:13 +0700

    saving

--- app/src/main/java/com/example/smartwall/SavingsActivity.java
+++ app/src/main/java/com/example/smartwall/SavingsActivity.java
@@ -1,26 +1,44 @@
 package com.example.smartwall;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.smartwall.MainActivity;
+import com.example.smartwall.R;
+import com.example.smartwall.createSavingActivity;
 
 public class SavingsActivity extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_savings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.saving), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_saving);
+
+        Button buttonAdd = findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang CreateSavingActivity khi nút "Thêm vào" được nhấn
+                Intent intent = new Intent(SavingsActivity.this, createSavingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Xử lý khi nhấn vào ImageView để quay về trang chủ
+        ImageView imageViewBackToHome = findViewById(R.id.imageViewBackToHome);
+        imageViewBackToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tạo Intent để quay về MainActivity (hoặc trang chủ của bạn)
+                Intent intent = new Intent(SavingsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Xóa các Activity trên đỉnh stack
+                startActivity(intent);
+                finish(); // Đóng Activity hiện tại
+            }
         });
     }
 }

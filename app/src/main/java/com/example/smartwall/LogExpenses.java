5bf462a596686c18b13691eb91b10c22bfd2a09f
THuanneee <94958911+THuanneee@users.noreply.github.com>
2024-06-27 00:22:29 +0700

    update

--- app/src/main/java/com/example/smartwall/LogExpenses.java
+++ app/src/main/java/com/example/smartwall/LogExpenses.java
@@ -1,24 +1,71 @@
 package com.example.smartwall;
 
-        import android.annotation.SuppressLint;
-        import android.os.Bundle;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import androidx.activity.EdgeToEdge;
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.core.graphics.Insets;
-        import androidx.core.view.ViewCompat;
-        import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.smartwall.model.Expenses;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class LogExpenses extends AppCompatActivity {
-    @SuppressLint("MissingInflatedId")
+    private IncomeAdapter incomeAdapter;
+    private ArrayList<Expenses> incomeList;
+    private ListView listViewHistory;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_activity_log_expenses);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.logExpenses), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listViewHistory = findViewById(R.id.listViewHistory);
+        incomeList = new ArrayList<>();
+        incomeAdapter = new IncomeAdapter(this, incomeList);
+        listViewHistory.setAdapter(incomeAdapter);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.child("income").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                incomeList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Expenses income = dataSnapshot.getValue(Expenses.class);
+                    if (income != null) {
+                        try {
+                            int incomeTotal = Integer.parseInt(income.getTotal());
+                            incomeList.add(income);
+                        } catch (NumberFormatException e) {
+                        }
+                    }
+                }
+                //totalIncomeTextView.setText(String.format("+ %,d VND", totalIncome));
+                incomeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
         });
+
+
     }
 }
--- app/src/main/java/com/example/smartwall/LogRevenue.java
+++ app/src/main/java/com/example/smartwall/LogRevenue.java
@@ -1,24 +1,149 @@
 package com.example.smartwall;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.smartwall.model.Expenses;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class LogRevenue extends AppCompatActivity {
+    private ExpenseAdapter expenseAdapter;
+    private ArrayList<Expenses> expenseList;
+    private ListView listViewExpense;
+    private DatabaseReference databaseReference;
+    private EditText search;
+    private ArrayList<Expenses> filteredExpenseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.fragment_activity_log_revenue);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        search = findViewById(R.id.search);
+        listViewExpense = findViewById(R.id.listViewHistory);
+        expenseList = new ArrayList<>();
+        expenseAdapter = new ExpenseAdapter(this, expenseList);
+        listViewExpense.setAdapter(expenseAdapter);
+        filteredExpenseList = new ArrayList<>();
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.child("expenses").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                expenseList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Expenses expense = dataSnapshot.getValue(Expenses.class);
+                    if (expense != null) {
+                        try {
+                            int expenseTotal = Integer.parseInt(expense.getTotal());
+                            expenseList.add(expense);
+                        } catch (NumberFormatException e) {
+                        }
+                    }
+                }
+                //totalExpenseTextView.setText(String.format("- %,d VND", totalExpense));
+                expenseAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(LogRevenue.this, "Error fetching expense data!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // N/A
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Thực hiện truy vấn dữ liệu từ Firebase dựa trên văn bản tìm kiếm
+                String searchText = s.toString().trim();
+                if (searchText.isEmpty()) {
+                    databaseReference.child("expenses").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            expenseList.clear();
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                Expenses expense = dataSnapshot.getValue(Expenses.class);
+                                if (expense != null) {
+                                    try {
+                                        int expenseTotal = Integer.parseInt(expense.getTotal());
+                                        expenseList.add(expense);
+                                    } catch (NumberFormatException e) {
+                                    }
+                                }
+                            }
+                            //totalExpenseTextView.setText(String.format("- %,d VND", totalExpense));
+                            expenseAdapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(LogRevenue.this, "Error fetching expense data!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    queryExpenses(searchText); // Nếu có văn bản tìm kiếm, thực hiện truy vấn Firebase
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // N/A
+            }
+        });
+
+
+
+    }private void queryExpenses(String searchText) {
+        Query query = databaseReference.child("expenses").orderByChild("category").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                expenseList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Expenses expense = dataSnapshot.getValue(Expenses.class);
+                    if (expense != null) {
+                        try {
+                            int expenseTotal = Integer.parseInt(expense.getTotal());
+                            expenseList.add(expense);
+                        } catch (NumberFormatException e) {
+                        }
+                    }
+                }
+                expenseAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(LogRevenue.this, "Error fetching expense data!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
+
+
 }
